b708576efed1fd7020325fcc9715cc4d80de09da
phan tran hong phuc <aladin1266@gmail.com>
2016-03-18 11:45:30 +0700

    new

---
+++ Web/Web/Controllers/CustomerController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [System.Web.Http.Cors.EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomerController : ApiController
+    {
+        private WebContext db = new WebContext();
+
+        // GET api/Customer
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }
+
+        // GET api/Customer/5
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> GetCustomer(long id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // PUT api/Customer/5
+        public async Task<IHttpActionResult> PutCustomer(long id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customer.CustomerID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Customer
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> PostCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (CustomerExists(customer.Email))
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                db.Customers.Add(customer);
+                await db.SaveChangesAsync();
+            }
+            
+            return CreatedAtRoute("DefaultApi", new { id = customer.CustomerID }, customer);
+        }
+
+        // DELETE api/Customer/5
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> DeleteCustomer(long id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            db.Customers.Remove(customer);
+            await db.SaveChangesAsync();
+
+            return Ok(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerExists(String email)
+        {
+            return db.Customers.Count(e => e.Email == email) > 0;
+        }
+        private bool CustomerExists(long ID)
+        {
+            return db.Customers.Count(e => e.CustomerID == ID) > 0;
+        }
+    }
+}
---
+++ Web/Web/Controllers/LaptopController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Web.Models;
+
+namespace Web.Controllers
+{
+     [System.Web.Http.Cors.EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LaptopController : ApiController
+    {
+        private WebContext db = new WebContext();
+
+        // GET api/Laptop
+        public IQueryable<Laptop> GetLaptops()
+        {
+            var laptops = db.Laptops.ToList();
+            return db.Laptops;
+        }
+
+        // GET api/Laptop/5
+        [ResponseType(typeof(Laptop))]
+        public async Task<IHttpActionResult> GetLaptop(string id)
+        {
+            Laptop laptop = await db.Laptops.FindAsync(id);
+            if (laptop == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(laptop);
+        }
+
+        // PUT api/Laptop/5
+        public async Task<IHttpActionResult> PutLaptop(string id, Laptop laptop)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != laptop.LaptopID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(laptop).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LaptopExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Laptop
+        [ResponseType(typeof(Laptop))]
+        public async Task<IHttpActionResult> PostLaptop(Laptop laptop)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Laptops.Add(laptop);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LaptopExists(laptop.LaptopID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = laptop.LaptopID }, laptop);
+        }
+
+        // DELETE api/Laptop/5
+        [ResponseType(typeof(Laptop))]
+        public async Task<IHttpActionResult> DeleteLaptop(string id)
+        {
+            Laptop laptop = await db.Laptops.FindAsync(id);
+            if (laptop == null)
+            {
+                return NotFound();
+            }
+
+            db.Laptops.Remove(laptop);
+            await db.SaveChangesAsync();
+
+            return Ok(laptop);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LaptopExists(string id)
+        {
+            return db.Laptops.Count(e => e.LaptopID == id) > 0;
+        }
+    }
+}
